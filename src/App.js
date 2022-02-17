8ecc9a572ec641a3f5b0a9b923f5a41243d3a82e
MartinSugasti <martinsugasti@gmail.com>
2022-02-17 09:57:54 +0100

    Add timer

--- src/App.js
+++ src/App.js
@@ -7,36 +7,70 @@ import items from './components/items';
 const App = () => {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-  const [cards] = useState(items);
+  const cards = items;
   const [clickedCardsIds, setClickedCardsIds] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
+  // resetTimer has the purpose of notifying Stats to reset the timer when it changes its value, but it doesn't matter its value.
+  const [resetTimer, setResetTimer] = useState(false);
 
   const handleClick = (cardId) => {
+    // it may or not be the first click. If it's not GameStarted is already true but it doesn't matter
+    setGameStarted(true);
+
     if (clickedCardsIds.includes(cardId)) {
-      if (highScore < score) {
-        alert(`Game over, but you just set a new high score of ${score}.`);
-        setHighScore(score);
-      } else {
-        alert(`Game over. Your final score is ${score}.`);
-      }
-      setClickedCardsIds([]);
-      setScore(0);
+      gameOver();
     } else {
       if (score + 1 === cards.length) {
-        alert('Congratulations! You just remembered all crests!');
-        setHighScore(score + 1);
-        setClickedCardsIds([]);
-        setScore(0);
+        completeGame();
       } else {
-        setClickedCardsIds([...clickedCardsIds, cardId]);
-        setScore(score + 1);
+        continueGame(cardId);
       }
     }
+  };
+
+  const gameOver = () => {
+    if (highScore < score) {
+      alert(`Game over, but you just set a new high score of ${score}.`);
+      setHighScore(score);
+    } else {
+      alert(`Game over. Your final score is ${score}.`);
+    }
+    resetGame();
+  };
+
+  const completeGame = () => {
+    alert('Congratulations! You just remembered all crests!');
+    setHighScore(score + 1);
+    resetGame();
+  };
+
+  const continueGame = (cardId) => {
+    setClickedCardsIds([...clickedCardsIds, cardId]);
+    setScore(score + 1);
+    setResetTimer(!resetTimer);
+  };
+
+  const resetGame = () => {
+    setClickedCardsIds([]);
+    setScore(0);
+    setGameStarted(false);
+    setResetTimer(!resetTimer);
+  };
+
+  const timesOut = () => {
+    gameOver();
   };
 
   return (
     <div>
       <Header />
-      <Stats highScore={highScore} score={score} />
+      <Stats
+        highScore={highScore}
+        score={score}
+        gameStarted={gameStarted}
+        timesOut={timesOut}
+        resetTimer={resetTimer}
+      />
       <Cards cards={cards} handleClick={handleClick} />
     </div>
   );
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,8 +1,34 @@
-function Stats({ highScore, score }) {
+import { useState, useEffect } from 'react';
+
+function Stats({ highScore, score, gameStarted, timesOut, resetTimer }) {
+  const startTime = 10;
+  const [timeRemaining, setTimeRemaining] = useState(startTime);
+
+  useEffect(() => {
+    if (gameStarted) {
+      const timer = setInterval(() => {
+        if (timeRemaining === 0) {
+          timesOut();
+        } else {
+          setTimeRemaining((timeRemaining) => timeRemaining - 1);
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  });
+
+  useEffect(() => {
+    setTimeRemaining(startTime);
+  }, [resetTimer]);
+
   return (
     <div className="text-center my-4">
       <h3>High Score: {highScore}</h3>
       <h3>Score: {score}</h3>
+      <h3>Timer: {timeRemaining}</h3>
     </div>
   );
 }
